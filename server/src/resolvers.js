345c69307cec9c7dcef365440227c564a70c2ed4
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2022-10-29 14:08:07 -0300

    Finishes server-side modifications

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,11 +4,17 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);;
+    }
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+    modules: ({ id }, __, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id)
+    }
   },
 };
 
